b90e438ccffacadb1712b7390dbdae0f11a3709a
Haotian Lyu <1938629638@qq.com>
2025-05-17 22:59:11 +0800

    format the document

--- src/hirag_mcp/hirag.py
+++ src/hirag_mcp/hirag.py
@@ -1,10 +1,9 @@
+import asyncio
 import logging
 import time
-import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass, field
 from typing import Optional
-from concurrent.futures import ThreadPoolExecutor
-
 
 from hirag_mcp._llm import gpt_4o_mini_complete, openai_embedding
 from hirag_mcp.chunk import BaseChunk, FixTokenChunk
@@ -19,15 +18,16 @@ from hirag_mcp.storage import (
 )
 
 logging.basicConfig(
-    level=logging.INFO, 
+    level=logging.INFO,
     format="%(asctime)s %(name)s %(levelname)s: %(message)s",
-    datefmt="%H:%M:%S"
+    datefmt="%H:%M:%S",
 )
 
 logging.getLogger("HiRAG").setLevel(logging.INFO)
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger("HiRAG")
 logger = logging.getLogger("HiRAG-BENCH")
+
 
 @dataclass
 class HiRAG:
@@ -60,48 +60,49 @@ class HiRAG:
             )
             kwargs["vdb"] = lancedb
         return cls(**kwargs)
-    
-    async def _process_document(self,document):
-        loop=asyncio.get_event_loop()
+
+    async def _process_document(self, document):
+        loop = asyncio.get_event_loop()
         with ThreadPoolExecutor() as pool:
-            chunks=await loop.run_in_executor(pool,self.chunker.chunk,document)
-        
-        await asyncio.gather(*[
-            self.vdb.upsert_text(
-                text_to_embed=chunk.page_content,
-                properties={
-                    "document_key": chunk.id,
-                    "text": chunk.page_content,
-                    **chunk.metadata.__dict__,
-                },
-                table_name="chunks",
-                mode="overwrite",
-            )
-            for chunk in chunks
-        ])
+            chunks = await loop.run_in_executor(pool, self.chunker.chunk, document)
 
-        entities=await self.entity_extractor.entity(chunks)
+        await asyncio.gather(
+            *[
+                self.vdb.upsert_text(
+                    text_to_embed=chunk.page_content,
+                    properties={
+                        "document_key": chunk.id,
+                        "text": chunk.page_content,
+                        **chunk.metadata.__dict__,
+                    },
+                    table_name="chunks",
+                    mode="overwrite",
+                )
+                for chunk in chunks
+            ]
+        )
 
-        await asyncio.gather(*[
-            self.vdb.upsert_text(
-                text_to_embed=entity.metadata.description,
-                properties={
-                    "document_key": entity.id,
-                    "text": entity.page_content,
-                    **entity.metadata.__dict__,
-                },
-                table_name="entities",
-                mode="overwrite",
-            )
-            for entity in entities
-        ])
+        entities = await self.entity_extractor.entity(chunks)
+
+        await asyncio.gather(
+            *[
+                self.vdb.upsert_text(
+                    text_to_embed=entity.metadata.description,
+                    properties={
+                        "document_key": entity.id,
+                        "text": entity.page_content,
+                        **entity.metadata.__dict__,
+                    },
+                    table_name="entities",
+                    mode="overwrite",
+                )
+                for entity in entities
+            ]
+        )
 
         relations = await self.entity_extractor.relation(chunks, entities)
 
-        await asyncio.gather(*[
-            self.gdb.upsert_relation(r)
-            for r in relations
-        ])
+        await asyncio.gather(*[self.gdb.upsert_relation(r) for r in relations])
 
     async def insert_to_kb(
         self,
@@ -111,10 +112,12 @@ class HiRAG:
         loader_configs: Optional[dict] = None,
     ):
         start_total = time.perf_counter()
-        documents=load_document(document_path,content_type,document_meta,loader_configs)
+        documents = load_document(
+            document_path, content_type, document_meta, loader_configs
+        )
 
-        tasks=[self._process_document(doc) for doc in documents]
-        
+        tasks = [self._process_document(doc) for doc in documents]
+
         await asyncio.gather(*tasks)
         total = time.perf_counter() - start_total
         logger.info(f"Total pipeline time: {total:.3f}s")
